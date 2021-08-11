58822a8fcbd9c700f4baa901782623465f61d92c
manivannan-selvarajan <ms.manivannan1963@gmail.com>
2021-08-11 19:12:12 +0530

    Add email Trustifi for mail confirmation

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,16 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# keys available in ~/.bashrc
+ActionMailer::Base.smtp_settings = {
+  :port           => '587',
+  :address        => ENV['TRUSTIFI_URL'],
+  :user_name      => ENV['TRUSTIFI_KEY'],
+  :password       => ENV['TRUSTIFI_SECRET'],
+  :domain         => 'herokuapp.com',
+  :authentication => :plain,
+  :enable_starttle_auto => true
+}
+
+ActionMailer::Base.delivery_method = :smtp
